9fa3cff95d37174bde955e006c02b2ecbb244634
Ldi569321 <81796049+Ldi569321@users.noreply.github.com>
2022-06-04 12:55:43 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,41 @@
-const RC = document.getElementById("rightClick");
-const LC = document.getElementById("leftClick");
+const btn = document.querySelectorAll("button");
 let slider = document.getElementById("slider");
 let innerSlider = document.querySelector(".slider-inner");
-let box = document.querySelectorAll(".box");
+const box = document.querySelectorAll(".box");
 let plus = 0;
-let count = -1;
+let frontCount = -1;
+let backCount;
 
 function Rclick() {
-  if (plus < 2600) {
-    plus += 200;
-    innerSlider.style.left = `-${plus}px`;
+  if (frontCount <= 6) {
+    frontCount++;
+    box[frontCount].classList.remove("fadeoutLeft"),
+      void box[frontCount].offsetWidth,
+      box[frontCount].classList.add("fadeoutLeft");
+    btn[0].disabled = true;
+    setTimeout(function () {
+      innerSlider.appendChild(box[frontCount]);
+      box[frontCount].classList.remove("fadeoutLeft");
+    }, 200);
+    setTimeout(function () {
+      btn[0].disabled = false;
+    }, 500);
+  } else {
+    box[0].classList.remove("fadeoutLeft"),
+      void box[0].offsetWidth,
+      box[0].classList.add("fadeoutLeft");
+    btn[0].disabled = true;
+    setTimeout(function () {
+      innerSlider.appendChild(box[0]);
+      box[0].classList.remove("fadeoutLeft");
+    }, 200);
+    setTimeout(function () {
+      btn[0].disabled = false;
+    }, 500);
+    return frontCount = 0;
   }
+
+  return backCount = frontCount;
 }
 
-function Lclick() {
-  if (plus > 0) {
-    plus -= 500;
-    innerSlider.style.left = `-${plus}px`;
-  }
-}
-
-RC.addEventListener("click", Rclick);
-LC.addEventListener("click", Lclick);
+btn[0].addEventListener("click", Rclick);
